f8fb2ed582f88ba3a972b1d861f26d747d2c58b0
Andrey <andre.kozloff2004@gmail.com>
2023-11-18 16:11:12 +0300

    task one point one

--- js/script.js
+++ js/script.js
@@ -1,93 +1,40 @@
-const myAwesomeArray = [[21,[1, 2]], [[3, 4],2,1,4], 5];
-
-// самая простая альтернатива Flat
-// console.log(myAwesomeArray.join().split(","))
-
-
-
-let users = [
-    {
-        name: 'Andrey', 
-        age: 20,
-        status:"active"
-    }, 
-    {
-        name: 'Mad', 
-        age: 15,
-        status:"active"
-    },
-    {
-        name: 'Igor', 
-        age: 10,
-        status:"active"
-    },
-    {
-        name: 'El', 
-        age: 41,
-        status:"passive"
-    },
-    {
-        name: 'Ashab', 
-        age: 91,
-        status:"active"
-    },
-]
-// console.log(users.every(item => item.age > 18))
-
-// Задание для every и some:
-// console.log(users.every(item => item.status == "active"))
-// console.log(users.some(item => item.status == "active"))
-
-// Задание для fill
-
-// function replaceValue(numbers, value){
-//     return numbers.fill(value)
+// item 1
+// function cortNumbers(arr) {
+//     let swapped;
+//     do {
+//         swapped = false;
+//         arr.forEach((item, index) => {
+//             if (item > arr[index + 1]) {
+//             let temp = item;
+//             arr[index] = arr[index + 1];
+//             arr[index + 1] = temp;
+//             swapped = true;
+//         }})
+//     } while (swapped);
+//     console.log(arr);
 // }
-// console.log(replaceValue([1,2,3,4], 1))
+// cortNumbers([10,9,8,7,6,5,4,3,2,1,0])
 
-// Задание для flat:
+// item 2
+// let text = "Вася любит информатику и любит петь";
+// text = text.split(" ")
+// text[0] = "Андрей"
+// console.log(text.join(" "))
 
-// const data = [[1,[10,11], 2, 3], [1, 3, 4], [4, 5, 6]];
-// console.log(data.flat(2))
 
-// Сложное комбинированное задание
+// item 5
+// let userChoice = prompt("Задай вопрос!")
 
-let orders = [
-    {
-        goods:["хлеб","греча","наушники"],
-        status:"доставлен",
-        id:1,
-        price:10000,
-    }, 
-    {
-        goods:["планшет","ноутбук"],
-        status:"доставлен",
-        id:2,
-        price:30000,
-    },
-    {
-        goods:["iphone XR"],
-        status:"доставлен",
-        id:3,
-        price:15000,
-    },
-    {
-        goods:[],
-        status:"в обработке",
-        id:4,
-        price:0,
-    }
-]
-//Используя `every` и `some`, определите:
-// - Все ли заказы доставлены (статус "доставлен")?
+// function mtRandom(min, max){
+//     // получаем рандоиное число
+//     return Math.floor(Math.random() * (max - min + 1))
+// }
 
-// console.log(orders.every(order => order.status == "доставлен"))
-
-// - Есть ли хотя бы один заказ с товарами, стоимость которых превышает определенную сумму?
-// console.log(orders.some(order => order.price >= 30000))
-
-// - Есть ли хотя бы один заказ со статусом "в обработке", но без товаров?
-
-// console.log(orders.some(order => {
-//     return order.goods.length == 0 && order.status == "в обработке";
-// }))
+// var textResponse = [
+//     "звучит неплохо",
+//     "да,это определенно надо сделать",
+//     "не думаю,что это хорошая идея",
+//     "может,не сегодня",
+//     "Компьютер говорит нет"
+// ]
+// alert(textResponse[mtRandom(0,textResponse.length - 1)])
